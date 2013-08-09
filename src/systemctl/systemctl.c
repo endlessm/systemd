746b44ca0653f67868c61c032fa5ec6a1213a052
Michael Stapelberg <michael@stapelberg.de>
2013-08-09 20:23:31 +0200

    Also redirect to update-rc.d when not using .service
    I.e. make systemctl enable rsyslog work (previously, only systemctl
    enable rsyslog.service)

--- src/systemctl/systemctl.c
+++ src/systemctl/systemctl.c
@@ -5024,9 +5024,6 @@ static int enable_sysv_units(const char *verb, char **args) {
 
                 name = args[f];
 
-                if (!endswith(name, ".service"))
-                        continue;
-
                 if (path_is_absolute(name))
                         continue;
 
@@ -5060,7 +5057,8 @@ static int enable_sysv_units(const char *verb, char **args) {
                         goto finish;
                 }
 
-                p[strlen(p) - strlen(".service")] = 0;
+                if (endswith(name, ".service"))
+                        p[strlen(p) - strlen(".service")] = 0;
                 found_sysv = access(p, F_OK) >= 0;
                 if (!found_sysv)
                         continue;
