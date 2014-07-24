3fb6987cca769bba2caf0291ea35d5961d557e82
Ansgar Burchardt <ansgar@debian.org>
2014-07-24 19:38:07 +0200

    [DEB] Include additional directories in ProtectSystem
    Gbp-Pq: Topic debian
    Gbp-Pq: Name Include-additional-directories-in-ProtectSystem.patch

--- src/core/namespace.c
+++ src/core/namespace.c
@@ -125,6 +125,10 @@ static const TargetMount protect_home_yes_table[] = {
 /* ProtectSystem=yes table */
 static const TargetMount protect_system_yes_table[] = {
         { "/usr",       READONLY,       false },
+        { "/lib",       READONLY,       true },
+        { "/lib64",     READONLY,       true },
+        { "/bin",       READONLY,       true },
+        { "/sbin",      READONLY,       true },
         { "/boot",      READONLY,       true  },
         { "/efi",       READONLY,       true  },
 };
@@ -132,6 +136,10 @@ static const TargetMount protect_system_yes_table[] = {
 /* ProtectSystem=full includes ProtectSystem=yes */
 static const TargetMount protect_system_full_table[] = {
         { "/usr",       READONLY,       false },
+        { "/lib",       READONLY,       true },
+        { "/lib64",     READONLY,       true },
+        { "/bin",       READONLY,       true },
+        { "/sbin",      READONLY,       true },
         { "/boot",      READONLY,       true  },
         { "/efi",       READONLY,       true  },
         { "/etc",       READONLY,       false },
