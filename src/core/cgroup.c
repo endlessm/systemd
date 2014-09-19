e05abe9f39263fee833a87834c06ad71228e4b61
Michal Sekletar <msekleta@redhat.com>
2014-09-19 17:14:10 +0200

    cgroup: don't trim cgroup trees created by someone else
    In cases when there is a cgroup tree in a controller hierarchy which was
    not created by us, but it looks like it was (i.e. cgroup path is the
    same as the one in systemd's named hierarchy) we shouldn't delete it.
    
    Origin: http://lists.freedesktop.org/archives/systemd-devel/2014-September/023276.html
    Bug-Debian: https://bugs.debian.org/777601
    
    Originally from debian/patches/cgroup-don-t-trim-cgroup-trees-created-by-someone-el.patch

--- src/core/cgroup.c
+++ src/core/cgroup.c
@@ -1056,7 +1056,7 @@ void unit_prune_cgroup(Unit *u) {
 
         is_root_slice = unit_has_name(u, SPECIAL_ROOT_SLICE);
 
-        r = cg_trim_everywhere(u->manager->cgroup_supported, u->cgroup_path, !is_root_slice);
+        r = cg_trim_everywhere(u->cgroup_realized_mask, u->cgroup_path, !is_root_slice);
         if (r < 0) {
                 log_debug_errno(r, "Failed to destroy cgroup %s, ignoring: %m", u->cgroup_path);
                 return;
