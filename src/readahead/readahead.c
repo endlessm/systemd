a65f460d62de3ddce72dde6fc1177745181ff894
John Hiesey <john@hiesey.com>
2015-09-24 16:14:41 -0700

    readahead: Add --pack-file agrument to systemd-readahead
    This allows the user to specify the pack file path explicitly
    instead of having it hardcoded to DIRECTORY/.readahead
    
    If this argument isn't specified, the pack file is stored at
    the old location.

--- src/readahead/readahead.c
+++ src/readahead/readahead.c
@@ -32,6 +32,7 @@
 #include "build.h"
 #include "readahead-common.h"
 
+const char *arg_pack_file;
 unsigned arg_files_max = 16*1024;
 off_t arg_file_size_max = READAHEAD_FILE_SIZE_MAX;
 usec_t arg_timeout = 2*USEC_PER_MINUTE;
@@ -42,6 +43,7 @@ static int help(void) {
                "Collect read-ahead data on early boot.\n\n"
                "  -h --help                 Show this help\n"
                "     --version              Show package version\n"
+               "     --pack-file=PATH       Custom location for pack file\n"
                "     --files-max=INT        Maximum number of files to read ahead\n"
                "     --file-size-max=BYTES  Maximum size of files to read ahead\n"
                "     --timeout=USEC         Maximum time to spend collecting data\n\n\n",
@@ -51,6 +53,7 @@ static int help(void) {
                "Replay collected read-ahead data on early boot.\n\n"
                "  -h --help                 Show this help\n"
                "     --version              Show package version\n"
+               "     --pack-file=PATH       Custom location for pack file\n"
                "     --file-size-max=BYTES  Maximum size of files to read ahead\n\n\n",
                program_invocation_short_name);
 
@@ -67,6 +70,7 @@ static int parse_argv(int argc, char *argv[]) {
 
         enum {
                 ARG_VERSION = 0x100,
+                ARG_PACKFILE,
                 ARG_FILES_MAX,
                 ARG_FILE_SIZE_MAX,
                 ARG_TIMEOUT
@@ -75,6 +79,7 @@ static int parse_argv(int argc, char *argv[]) {
         static const struct option options[] = {
                 { "help",          no_argument,       NULL, 'h'                },
                 { "version",       no_argument,       NULL, ARG_VERSION        },
+                { "pack-file",     required_argument, NULL, ARG_PACKFILE       },
                 { "files-max",     required_argument, NULL, ARG_FILES_MAX      },
                 { "file-size-max", required_argument, NULL, ARG_FILE_SIZE_MAX  },
                 { "timeout",       required_argument, NULL, ARG_TIMEOUT        },
@@ -97,6 +102,12 @@ static int parse_argv(int argc, char *argv[]) {
                         puts(PACKAGE_STRING);
                         puts(SYSTEMD_FEATURES);
                         return 0;
+
+                case ARG_PACKFILE:
+                        arg_pack_file = strdup(optarg);
+                        if (!arg_pack_file)
+                                return log_oom();
+                        break;
 
                 case ARG_FILES_MAX:
                         if (safe_atou(optarg, &arg_files_max) < 0 || arg_files_max <= 0) {
