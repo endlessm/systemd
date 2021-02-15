28c5daab8c238703903c628c514c349c9edfdb2b
Michael Biebl <biebl@debian.org>
2021-02-16 00:18:50 +0100

    [DEB] Downgrade a couple of warnings to debug
    If a package still ships only a SysV init script or if a service file or
    tmpfile uses /var/run, downgrade those messages to debug. We can use
    lintian to detect those issues.
    For service files and tmpfiles in /etc, keep the warning, as those files
    are typically added locally and aren't checked by lintian.
    
    Closes: #981407

--- src/tmpfiles/tmpfiles.c
+++ src/tmpfiles/tmpfiles.c
@@ -2538,6 +2538,7 @@ static int specifier_expansion_from_arg(Item *i) {
 static int patch_var_run(const char *fname, unsigned line, char **path) {
         const char *k;
         char *n;
+        int log_level;
 
         assert(path);
         assert(*path);
@@ -2563,7 +2564,8 @@ static int patch_var_run(const char *fname, unsigned line, char **path) {
         /* Also log about this briefly. We do so at LOG_NOTICE level, as we fixed up the situation automatically, hence
          * there's no immediate need for action by the user. However, in the interest of making things less confusing
          * to the user, let's still inform the user that these snippets should really be updated. */
-        log_syntax(NULL, LOG_NOTICE, fname, line, 0, "Line references path below legacy directory /var/run/, updating %s → %s; please update the tmpfiles.d/ drop-in file accordingly.", *path, n);
+        log_level = path_startswith(fname, "/etc") ? LOG_NOTICE : LOG_DEBUG;
+        log_syntax(NULL, log_level, fname, line, 0, "Line references path below legacy directory /var/run/, updating %s → %s; please update the tmpfiles.d/ drop-in file accordingly.", *path, n);
 
         free_and_replace(*path, n);
 
